21fdb3835b859ad0164c3898e6208d8d70bb1510
Jeremy Goodman <jeremygoodman52@gmail.com>
2023-04-21 14:34:31 -0500

    main swap

--- Tabloid/client/src/components/PostComments.js
+++ Tabloid/client/src/components/PostComments.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-<<<<<<< HEAD
 import { Card, CardBody } from "reactstrap";
 import { deleteComment } from "../modules/commentManager";
 import { me } from "../modules/authManager";
@@ -33,24 +32,5 @@ export const PostComments = ({ commentsOnPost, getComments }) => {
         </li>
         </>
     })}
-=======
-import { Card, CardBody, CardTitle } from "reactstrap";
-import { getPostComments } from "../modules/commentManager";
-
-
-
-export const PostComments = ({ commentsOnPost }) => {
-
-
-    return <>
-        <CardTitle tag='h5'>Comments</CardTitle>
-        {
-            commentsOnPost.map(comment => {
-                return <li key={comment.id}>
-                    {comment.content}
-                </li>
-            })
-        }
->>>>>>> main
     </>
 }
